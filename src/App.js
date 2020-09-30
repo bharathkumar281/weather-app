73cf99d079e6c6de3e6ee00e7cc5c50abec2dd89
bharathkumar281 <munagaribharath1999@gmail.com>
2020-09-30 18:15:58 +0530

    fixed router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className="App">
           <Main />
         </div>
-      </HashRouter>
+      </BrowserRouter>
     );
   }
 }
